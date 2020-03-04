26712e3ea99b4307ac1765378647100de30d1bbe
gyim <gyim1345@gmail.com>
2020-03-04 21:45:25 +0900

    Fix upload to work
    Add import promisify
    fix directory for save location of file

--- src/routes/upload.route.js
+++ src/routes/upload.route.js
@@ -1,19 +1,33 @@
 import express from 'express';
 import postStore from '../postingStore';
+import postSchemaModel from '../model/post';
+import { promisify } from 'util';
+
 
 const router = express.Router();
 
 
-router.post('/upload', async (req, res) => {
-    if (req.files === null) {
-      return res.status(400).json({ msg: 'No file uploaded' });
-    }
-    
-    const file = req.files.file;
-    const { input, user, inputTag } = req.body;
-    try {
-      await promisify(file.mv)(`${__dirname}/static/images/${file.name}`)
-      
+
+
+router.post('/', async (req, res) => {
+  if (req.files === null) {
+    return res.status(400).json({ msg: 'No file uploaded' });
+  }
+  const file = req.files.file;
+  const { input, user, inputTag } = req.body;
+  try {
+    await promisify(file.mv)(`/workspace/Project/snsAPI/src/static/images/${file.name}`)
+//     let postModel = new postSchemaModel();
+//     postModel.id = 1231;
+//     postModel.title = 'title';
+//     postModel.imageUrl = 'contetna';
+//     postModel.userName = 'usernameasd';
+//     postModel.like =['suck', 'balls'];
+//     postModel.tag=[];
+//     postModel.save();
+// console.log('asdasdsad')
+// console.log('asdaspromisify')
+
       const posts = postStore.createPost(
         input, 
         user, `http://localhost:3000/static/images/${file.name}`, inputTag);
