3461fc77b5cea72c3bf007f9e578a91b26be45d9
gyim <gyim1345@gmail.com>
2020-03-18 21:09:54 +0900

    Add unit test for register service

---
+++ src/tests/unit/Register.spec.js
@@ -0,0 +1,42 @@
+import register from '../../services/register'
+import userStore from '../../repository/userStore'
+
+describe('Register', () => {
+
+    describe('Registration', () => {
+        let id;
+        let password;
+        beforeAll(() => {
+            password = 1;
+     })
+
+        it('check id is not within a-zA-Z0-9@ range', async()=> {
+            id='#'
+            const response = await register.Registration(id, password)
+            expect(response.Message).toBe('Check Input');
+            expect(response.status).toBe(false);
+        })
+
+
+        describe('id is within a-zA-Z0-9@ range', ()=> {
+            
+            it('id is already registered', async() => {
+                userStore.checkIdIsRegistered = jest.fn().mockResolvedValue(false);
+                const response = await register.Registration(id, password)
+                expect(response.Message).toBe('Check Input');
+                expect(response.status).toBe(false);
+            })
+
+            it('register id with password', async() => {
+              id='@'
+              userStore.checkIdIsRegistered = jest.fn().mockResolvedValue(true);
+              userStore.createUser = jest.fn().mockResolvedValue();  
+              const response = await register.Registration(id, password)
+              expect(response.Message).toBe('Registered')
+              expect(response.status).toBe(true)
+        })
+    })
+
+
+    })
+})
