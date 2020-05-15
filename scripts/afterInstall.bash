ea405be2008b828acbbc7d67e3b1fdd1562eeadc
gyim <gyim1345@gmail.com>
2020-05-15 18:17:52 +0900

    test 24

--- scripts/afterInstall.bash
+++ scripts/afterInstall.bash
@@ -2,5 +2,5 @@ cd /home/ubuntu/build
 npm install
 mkdir config
 cd /
-cp /env/.env.production /home/ubuntu/build
-cp /env/awsconfig.json /home/ubuntu/build/config
+cp /env/.env.production /home/ubuntu/build/
+cp /env/awsconfig.json /home/ubuntu/build/config/
