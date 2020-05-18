5deca50468ca7134401bcd900019549440f701d3
gyim <gyim1345@gmail.com>
2020-05-18 19:09:31 +0900

    test bash 3

--- scripts/afterInstall.bash
+++ scripts/afterInstall.bash
@@ -1,8 +1,8 @@
-cd /home/ubuntu/build
-npm install
-mkdir config
-cd /
-cp /env/.env.production /home/ubuntu/build/
-cp /env/awsconfig.json /home/ubuntu/build/config/
+# cd /home/ubuntu/build
+# npm install
+# mkdir config
+# cd /
+# cp /env/.env.production /home/ubuntu/build/
+# cp /env/awsconfig.json /home/ubuntu/build/config/
 docker pull gyim1345/snsapi:latest
-docker run --env-file /env/.env.production .en--publish 8383:8383 --detach --name bb gyim1345/snsapi:latest
+docker run --env-file /env/.env.production .en--publish 8383:8383 --detach --name bb gyim1345/snsapi:latest /bin/bash
