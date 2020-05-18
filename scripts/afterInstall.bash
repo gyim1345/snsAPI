80afe7a766ce548c8553520bc161582aa897ebd5
gyim <gyim1345@gmail.com>
2020-05-18 18:56:51 +0900

    test bash 1

--- scripts/afterInstall.bash
+++ scripts/afterInstall.bash
@@ -5,3 +5,4 @@ cd /
 cp /env/.env.production /home/ubuntu/build/
 cp /env/awsconfig.json /home/ubuntu/build/config/
 docker pull gyim1345/snsapi
+docker run --env-file /env/.env.production .en--publish 8383:8383 --detach --name bb gyim1345/snsapi:latest
