031448020b108b5171291c12b9aed8ad8de7be18
gyim <gyim1345@gmail.com>
2020-05-18 10:28:54 +0900

    test bash

--- scripts/beforeInstall.bash
+++ scripts/beforeInstall.bash
@@ -2,3 +2,5 @@ if [ -d /home/ubuntu/build ]; then
     rm -rf /home/ubuntu/build
 fi
 mkdir -vp /home/ubuntu/build
+
+docker rmi -f $(docker images --format '{{.Repository}}:{{.Tag}}' --filter=reference='gyim1345/snsapi:latest')
