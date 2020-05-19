22e5d620ae6e4f17f0283866659f01f91131a149
gyim <gyim1345@gmail.com>
2020-05-19 11:34:28 +0900

    test complete workflow 6
    fix afterInstall.bash

--- scripts/afterInstall.bash
+++ scripts/afterInstall.bash
@@ -6,4 +6,5 @@
 # cp /env/awsconfig.json /home/ubuntu/build/config/
 sudo docker pull gyim1345/snsapi:latest
 sudo docker run --env-file /env/.env.production --publish 8383:8383 -it --detach --name ab gyim1345/snsapi:latest /bin/bash
-# docker exec -it $(docker ps -a -q --filter="ancestor=gyim1345/snsapi:1.1") /bin/bash
+docker exec -it $(docker ps -a -q --filter="ancestor=gyim1345/snsapi:1.1") /bin/bash
+npm start
