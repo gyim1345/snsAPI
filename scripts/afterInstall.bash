314e2edee8852224ec59ee3a85d22f9ed55af7dd
gyim <gyim1345@gmail.com>
2020-05-19 11:52:12 +0900

    test complete workflow 8

--- scripts/afterInstall.bash
+++ scripts/afterInstall.bash
@@ -7,4 +7,5 @@
 sudo docker pull gyim1345/snsapi:latest
 sudo docker run --env-file /env/.env.production --publish 8383:8383 -it --detach --name ab gyim1345/snsapi:latest /bin/bash
 docker exec -it $(docker ps -a -q --filter="ancestor=gyim1345/snsapi:latest") /bin/bash
+mkdir asdqwe
 npm start
