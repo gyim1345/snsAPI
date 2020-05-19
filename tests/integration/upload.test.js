06db318667a85671d0c0e25d699c2b755fd3582b
gyim <gyim1345@gmail.com>
2020-05-19 14:41:13 +0900

    test integration test on githubAction 5

--- tests/integration/upload.test.js
+++ tests/integration/upload.test.js
@@ -59,67 +59,66 @@ describe('/upload', () => {
                 .attach('files', 'static/images/1.jpg')
                 .field({ input: 'newInput', inputTag: 'newInputTag' })
                 .set('Cookie', cookie)
-            console.log(body)
-            // expect(body).toEqual(expect.objectContaining(uploadedFile))
-            // expect(body.posts).toEqual(expect.objectContaining(posts))
+            expect(body).toEqual(expect.objectContaining(uploadedFile))
+            expect(body.posts).toEqual(expect.objectContaining(posts))
         })
 
-    //     describe('when internal error from server', () => {
-    //         beforeEach(() => {
-    //             postStore.createPost = jest.fn().mockRejectedValue(new Error('Somthing wrong'))
-    //         });
+        describe('when internal error from server', () => {
+            beforeEach(() => {
+                postStore.createPost = jest.fn().mockRejectedValue(new Error('Somthing wrong'))
+            });
 
-    //         it('returns status code of 500 and message of Internal server error', async () => {
-    //             const { body, statusCode } = await request(app)
-    //                 .post('/upload')
-    //                 .attach('files', 'static/images/1.jpg')
-    //                 .field({ input: 'newInput', inputTag: 'newInputTag' })
-    //                 .set('Cookie', cookie)
+            it('returns status code of 500 and message of Internal server error', async () => {
+                const { body, statusCode } = await request(app)
+                    .post('/upload')
+                    .attach('files', 'static/images/1.jpg')
+                    .field({ input: 'newInput', inputTag: 'newInputTag' })
+                    .set('Cookie', cookie)
                     
-    //             expect(statusCode).toBe(500);
-    //             expect(body.message).toBe('Internal server error');
-    //         })
-    //     })
-    // })
+                expect(statusCode).toBe(500);
+                expect(body.message).toBe('Internal server error');
+            })
+        })
+    })
 
-    // describe('PATCH /userImage', () => {
+    describe('PATCH /userImage', () => {
 
-    //     it('edits Image and returns status code of 200 and a message of done', async () => {
-    //         const { body, statusCode } = await request(app)
-    //             .patch('/upload/userImage')
-    //             .attach('files', 'static/images/1.jpg')
-    //             .set('Cookie', cookie)
+        it('edits Image and returns status code of 200 and a message of done', async () => {
+            const { body, statusCode } = await request(app)
+                .patch('/upload/userImage')
+                .attach('files', 'static/images/1.jpg')
+                .set('Cookie', cookie)
 
-    //         expect(statusCode).toBe(200);
-    //         expect(body).toBe('done');
-    //     })
+            expect(statusCode).toBe(200);
+            expect(body).toBe('done');
+        })
 
 
 
-    //     describe('when internal error from server', () => {
-    //         beforeEach(() => {
-    //             userStore.editUserImage = jest.fn().mockRejectedValue(new Error('Somthing wrong'))
-    //         });
+        describe('when internal error from server', () => {
+            beforeEach(() => {
+                userStore.editUserImage = jest.fn().mockRejectedValue(new Error('Somthing wrong'))
+            });
 
-    //         it('returns status code of 500 and message of Internal server error', async () => {
-    //             const { body, statusCode } = await request(app)
-    //                 .patch('/upload/userImage')
-    //                 .attach('files', 'static/images/1.jpg')
-    //                 .set('Cookie', cookie)
+            it('returns status code of 500 and message of Internal server error', async () => {
+                const { body, statusCode } = await request(app)
+                    .patch('/upload/userImage')
+                    .attach('files', 'static/images/1.jpg')
+                    .set('Cookie', cookie)
 
-    //             expect(statusCode).toBe(500);
-    //             expect(body.message).toBe('Internal server error');
-    //         })
-    //     })
-    // })
-    // describe('when no file is sent', () => {
-    //     it('returns status code of 401 and message of Internal server error', async () => {
-    //         const { statusCode, body} = await request(app)
-    //             .patch('/upload/userImage')
-    //             .set('Cookie', cookie)
+                expect(statusCode).toBe(500);
+                expect(body.message).toBe('Internal server error');
+            })
+        })
+    })
+    describe('when no file is sent', () => {
+        it('returns status code of 401 and message of Internal server error', async () => {
+            const { statusCode, body} = await request(app)
+                .patch('/upload/userImage')
+                .set('Cookie', cookie)
 
-    //         expect(statusCode).toBe(401);
-    //         expect(body).toBe('No file uploaded');
-    // })
+            expect(statusCode).toBe(401);
+            expect(body).toBe('No file uploaded');
+    })
     })
 })
